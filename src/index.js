f031bee324e1aeee8ef76a7f9d138593c8d5fa6a
Ribich1 <121463488+Ribich1@users.noreply.github.com>
2023-10-20 14:57:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ import { App } from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
-  <HashRouter>
+      <BrowserRouter>
       <App />
-  </HashRouter>
-   
-  </React.StrictMode>
+      </BrowserRouter>
+
+   </React.StrictMode>
 );
